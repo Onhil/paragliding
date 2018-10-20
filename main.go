196277fa25338c60f5b1791a8ad78c106b327b57
Onhil <onhilx@gmail.com>
2018-10-20 11:52:21 +0200

    http routes

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+)
+
+var startTime time.Time
+
+func main() {
+	startTime = time.Now()
+	dbInit()
+
+	router := chi.NewRouter()
+	router.Route("/paragliding", func(r chi.Router) {
+		r.Route("/api", func(r chi.Router) {
+			r.Get("/", getAPI)
+			r.Route("/track", func(r chi.Router) {
+				r.Get("/", getTrackIDs)
+				r.Post("/", postTrack)
+				r.Route("/{id:[0-9]+}", func(r chi.Router) {
+					r.Get("/", getTrack)
+					r.Get("/{field:[A-Za-z_]+}/", getTrackField)
+				})
+			})
+			r.Route("/ticker", func(r chi.Router) {
+				//r.Get("/latest/", )
+				//r.get("/{timestamp:[0-9]+}/", )
+			})
+			r.Route("/weebhook", func(r chi.Router) {
+				r.Route("/new_track", func(r chi.Router) {
+					//r.Post("/", )
+					r.Route("/webhook_id:[0-9]+}", func(r chi.Router) {
+						//r.Get("/", )
+						//r.Delete("/", )
+					})
+				})
+			})
+		})
+		r.Route("/admin", func(r chi.Router) {
+			r.Route("/api", func(r chi.Router) {
+				//r.Get("/tracks_count/", )
+				//r.Delete("/tracks/", )
+			})
+		})
+	})
+	log.Fatal(http.ListenAndServe(GetPort(), router)) // set listen port
+}
